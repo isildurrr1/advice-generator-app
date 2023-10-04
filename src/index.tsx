c50394caf0f262448a22292e5ff3dcb9ecace14b
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-10-04 19:38:24 +0700

    normalize to sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.sass';
-import './vendor/normalize.css'
+import './vendor/normalize.sass'
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
